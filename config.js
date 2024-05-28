fbcabfab4b5546ccbbd2a900e6aa3d8814786c7b
Can <63351166+fastuptime@users.noreply.github.com>
2024-05-28 16:45:30 +0300

    Update config.js
    <3

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: "mongodb://localhost:27017",
-    clientID: "858317235856343060",
-    clientSecret: "6UgxMBYKFLTFVvvgqpPNK8ZRIoTv_uVV",
-    callbackURL: "https://mylocalhost.speedsmm.com/auth/callback",
+    clientID: "xxxx",
+    clientSecret: "xxxx",
+    callbackURL: "https://xxxxx.com/auth/callback",
 };
